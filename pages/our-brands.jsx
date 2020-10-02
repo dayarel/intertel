c657bb3bb9af789901b0659e3beffdeb66de3fbb
David Cortes <carvanasar@gmail.com>
2020-10-02 16:19:45 +0200

    Created pages

---
+++ pages/our-brands.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ourBrands = () => {
+  return (
+    <div>
+      <h1>ourBrands</h1>
+    </div>
+  );
+};
+
+export default ourBrands;
---
+++ pages/trade-form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const tradeForm = () => {
+  return (
+    <div>
+      <h1>Trade form</h1>
+    </div>
+  );
+};
+
+export default tradeForm;
